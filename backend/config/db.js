8e12accccd77e54f7b5a15c6a6a40e8d00bfb3db
Prachi Barhate <prachibarhate23@gmail.com>
2025-04-19 18:47:56 +0530

    changes done

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,11 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect('mongodb://localhost:27017/padhakoo', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
+    
     console.log('MongoDB Connected...');
   } catch (err) {
     console.error('Database connection error:', err.message);
